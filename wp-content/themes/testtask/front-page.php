5c9b2352858c14913793a9cd6b2c6f36b12ac98e
Kate Levytska <kate.levytska@tstechpro.com>
2022-02-01 22:50:08 +0200

    Added slider section and fixed styles

--- wp-content/themes/testtask/front-page.php
+++ wp-content/themes/testtask/front-page.php
@@ -1,9 +1,10 @@
 <?php
 get_header(); ?>
 
-<div class="form">
-    <?php echo do_shortcode('[contact-form-7 id="6" title="Contact form 1"]') ?>
-</div>
+<aside class="form">
+        <?php echo do_shortcode('[contact-form-7 id="6" title="Contact form 1"]') ?>
+</aside>
+
 <section class="sliderSection">
     <div class="sliderImages">
         <h2 class="sliderSection__title"><?php the_field('section2_title') ?></h2>
@@ -18,22 +19,22 @@ get_header(); ?>
     </div>
 
     <div class="sliderReviews">
-        <h2 class="sliderSection__title"><?php the_field('section2_title2') ?></h2>
+        <h2 class="sliderSection__title sliderSection__title-bottom"><?php the_field('section2_title2') ?></h2>
         <?php if (have_rows('reviews_slider')) : ?>
-            <div id="sliderReviews" >
+            <div id="sliderReviews">
                 <?php while (have_rows('reviews_slider')) : the_row(); ?>
                     <div>
                         <div class="sliderReviews__raiting">
-                        <?php
-                        $i = 1;
-                        $raiting_count = get_sub_field('raiting');
-                        while ($i <= $raiting_count ) {
-                            echo "<img src='" . get_template_directory_uri() . "/img/star.svg'>";
-                            $i++;
-                        }
-                        ?>
+                            <?php
+                            $i = 1;
+                            $raiting_count = get_sub_field('raiting');
+                            while ($i <= $raiting_count) {
+                                echo "<img src='" . get_template_directory_uri() . "/img/star.svg' class='sliderReviews__star'>";
+                                $i++;
+                            }
+                            ?>
                         </div>
-                        
+
                         <p class="sliderReviews__text">"<?php the_sub_field('review'); ?>"</p>
                         <p class="sliderReviews__text">– <?php the_sub_field('author'); ?></p>
                     </div>
@@ -42,9 +43,10 @@ get_header(); ?>
         <?php endif; ?>
     </div>
     <div class="sliderSection__bottom">
-    <p><?php the_field('section2_description') ?></p>
-    <a href="#" class="sliderSection__link"><?php the_field('section2_button') ?></a>
+        <p class="sliderSection__text"><?php the_field('section2_description') ?></p>
+        <a href="#" class="sliderSection__link"><?php the_field('section2_button') ?></a>
     </div>
 </section>
+
 <?php
 get_footer(); ?>
--- wp-content/themes/testtask/header.php
+++ wp-content/themes/testtask/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,48 +23,29 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'testtask' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="header">
+			<div class="branding">
 				<?php
-			else :
+				the_custom_logo();
+				if (is_front_page() && is_home()) :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$testtask_description = get_bloginfo( 'description', 'display' );
-			if ( $testtask_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $testtask_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-		<section>
-<div><?php the_field ('section1_banner-image')?></div>
-<div><?php the_field ('section1_banner-image2')?></div>
-<p><?php the_field ('section1_phone')?></p>
-<div><?php the_field ('section1_logo')?></div>
-<h1><?php the_field ('section1_title')?></h1>
-<p><?php the_field ('section1_description1')?></p>
-<p><?php the_field ('section1_description2')?></p>
-</section>  
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'testtask' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<?php
+				endif;
+				$testtask_description = get_bloginfo('description', 'display');
+				if ($testtask_description || is_customize_preview()) :
+				?>
+				<?php endif; ?>
+			</div><!-- .branding -->
+			<div style="background-image: url('<?php the_field('section1_banner-image') ?>')" class="header__banner"></div>
+			<div style="background-image: url('<?php the_field('section1_banner-image2') ?>')" class="header__banner-overlay"></div>
+			<p class="header__phone"><?php the_field('section1_phone') ?></p>
+			<div class="header__text">
+				<h1 class="header__title"><?php the_field('section1_title') ?></h1>
+				<p class="header__item"><?php the_field('section1_description1') ?></p>
+				<p class="header__description"><?php the_field('section1_description2') ?></p>
+			</div>
+		</header><!-- #masthead -->
