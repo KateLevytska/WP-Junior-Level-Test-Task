82b1ac1dde66ae508f9e98bfaa248fd002eb68eb
Kate Levytska <kate.levytska@tstechpro.com>
2022-02-04 10:47:19 +0200

    Fixed functions.php

--- wp-content/themes/testtask/functions.php
+++ wp-content/themes/testtask/functions.php
@@ -98,17 +98,6 @@ if (function_exists('acf_add_options_page')) {
 		'redirect'		=> false
 	));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Header Settings',
-		'menu_title'	=> 'Header',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
-		'menu_title'	=> 'Footer',
-		'parent_slug'	=> 'theme-general-settings',
-	));
 }
 
 add_action('after_setup_theme', 'testtask_setup');
