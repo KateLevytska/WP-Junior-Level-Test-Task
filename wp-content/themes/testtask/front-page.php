b7b958dcdb424b680946c7e133d300550803ed47
Kate Levytska <kate.levytska@tstechpro.com>
2022-02-09 15:50:36 +0200

    17) add a condition for an empty slider

--- wp-content/themes/testtask/front-page.php
+++ wp-content/themes/testtask/front-page.php
@@ -32,54 +32,61 @@ get_header(); ?>
 </aside>
 
 <section class="sliderSection">
-    <div class="sliderImages">
-        <h2 class="sliderSection__title"><?php the_field('sliderSection_title') ?></h2>
-        <?php 
+
+    <?php
     $images = get_field('image_slider');
-    if( $images ): ?>
-        <div id="sliderImages">
-            <?php foreach( $images as $image ): ?>
-                <div style="background-image: url('<?php echo esc_url($image['sizes']['medium']); ?>')" class="sliderImages__item">
-                </div>
-            <?php endforeach; ?>
-        </div>
-        <?php endif; ?>
-    </div>
+    if ($images) : ?>
 
-    <div class="sliderReviews">
-        <h2 class="sliderSection__title sliderSection__title-bottom"><?php the_field('sliderSection_title2') ?></h2>
-
-        <div id="sliderReviews">
+        <div class="sliderImages">
+            <h2 class="sliderSection__title"><?php the_field('sliderSection_title') ?></h2>
             <?php
-            global $post;
-            $myposts = get_posts(array(
-                'post_type' => 'testimonials'
-            ));
-            foreach ($myposts as $post) {
-                setup_postdata($post);
-            ?>
-                <div>
-                    <div class="sliderReviews__raiting">
-                        <?php
-                        $i = 1;
-                        $raiting_count = get_field('raiting');
-                        while ($i <= $raiting_count) {
-                            echo "<img src='" . get_template_directory_uri() . "/img/star.svg' class='sliderReviews__star' alt='raiting'>";
-                            $i++;
-                        }
-                        ?>
-                    </div>
-                    <div class="sliderReviews__content">
-                        <p class="sliderReviews__text"><?php echo get_the_content(); ?></p>
-                        <p class="sliderReviews__text">- <?php the_title(); ?></p>
-                    </div>
+            if ($images) : ?>
+                <div id="sliderImages">
+                    <?php foreach ($images as $image) : ?>
+                        <div style="background-image: url('<?php echo esc_url($image['sizes']['medium']); ?>')" class="sliderImages__item">
+                        </div>
+                    <?php endforeach; ?>
                 </div>
-            <?php
-            }
-            wp_reset_postdata();
-            ?>
+            <?php endif; ?>
         </div>
-    </div>
+    <?php endif; ?>
+
+    <?php $myposts = get_posts(array(
+        'post_type' => 'testimonials'
+    ));
+    ?>
+    <?php if ($myposts) : ?>
+        <div class="sliderReviews">
+            <h2 class="sliderSection__title sliderSection__title-bottom"><?php the_field('sliderSection_title2') ?></h2>
+            <div id="sliderReviews">
+                <?php
+                foreach ($myposts as $post) {
+                    setup_postdata($post);
+                ?>
+                    <div>
+                        <div class="sliderReviews__raiting">
+                            <?php
+                            $i = 1;
+                            $raiting_count = get_field('raiting');
+                            while ($i <= $raiting_count) {
+                                echo "<img src='" . get_template_directory_uri() . "/img/star.svg' class='sliderReviews__star' alt='raiting'>";
+                                $i++;
+                            }
+                            ?>
+                        </div>
+                        <div class="sliderReviews__content">
+                            <p class="sliderReviews__text"><?php echo get_the_content(); ?></p>
+                            <p class="sliderReviews__text">- <?php the_title(); ?></p>
+                        </div>
+                    </div>
+                <?php
+                }
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+    <?php endif; ?>
+    
     <div class="sliderSection__bottom">
         <p class="sliderSection__text"><?php the_field('sliderSection_description') ?></p>
         <a href="#" class="sliderSection__link"><?php the_field('sliderSection_button') ?></a>
